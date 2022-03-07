c6877933771cd93320570bfebbaf3db74115d590
Mykal Machon <mykal@tinybox.dev>
2022-03-07 21:36:00 +0000

    feat: trying to setup fake-indexeddb
    indexeddb is not mocked in jest. I'm trying to mock it using fake-indexeddb

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,11 @@ import { assert, describe, it } from 'vitest';
 import { screen } from '@testing-library/dom';
 
 
+
+
+import { init } from '../src/index'
+
+
 describe('Placeholders', () => {
   it('fires all hooks', () => {
     assert.equal(2, 2);
@@ -12,6 +17,9 @@ describe('Placeholders', () => {
       <span data-testid="not-empty"><span data-testid="empty"></span></span>
       <div data-testid="visible">Visible Example</div>
     `
+
+    // init({}) this isn't working because I am having trouble mocking indexedDB
+    // still trynna figure it out: https://www.npmjs.com/package/fake-indexeddb
 
     expect(screen.queryByTestId('not-empty')).not.toBeEmptyDOMElement()
     expect(screen.getByText('Visible Example')).toBeVisible()
--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,9 @@ module.exports = defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./vitest-setup.js']
+    setupFiles: ['./vitest-setup.ts'],
+    transformMode: {
+      web: [/\.[jt]sx$/],
+    },
   },
 });
